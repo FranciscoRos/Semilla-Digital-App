194dd1488d473544866cadee121cdfb470a45ab6
FranciscoRos <L21390377@chetumal.tecnm.mx>
2025-11-23 19:20:18 -0500

    cambio de ip solo

--- core-data/src/main/java/com/semilladigital/app/core/data/di/NetworkModule.kt
+++ core-data/src/main/java/com/semilladigital/app/core/data/di/NetworkModule.kt
@@ -17,7 +17,7 @@ import javax.inject.Singleton
 object NetworkModule {
 
     // 1. Define la URL base de tu API de Laravel
-    private const val BASE_URL = "http:192.168.0.188:8000/api/" // <-- IP LOCAL
+    private const val BASE_URL = "http:192.168.1.67:8000/api/" // <-- IP LOCAL
 
     // 2. Provee un interceptor de logs (para ver las llamadas en el Logcat)
     @Provides
